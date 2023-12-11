9fc7e6e46d1e61c2ba98db527db6be8d64916f14
ROHIT FRANCIS <108980398+Rohit909-creator@users.noreply.github.com>
2023-12-11 07:26:05 +0530

    Update example.py

--- example.py
+++ example.py
@@ -20,7 +20,7 @@ for dat in data:
 
 # print(len(data))
 # exit(0)
-context_length = 127
+context_length = 127*50
 batch_size = 32
 n_embs = 512
 mkdata = MakeData(text, context_length, batch_size)
@@ -31,10 +31,18 @@ device = torch.device(dev)
 m = TLM(mkdata.vocab_size, context_length, n_embs)
 m.to(device)
 
-trainer = Trainer(10, 2, device=dev)
+trainer = Trainer(5000, 1000, learning_rate = 0.0001, device=dev)
 trainer.train(mkdata,m)
 
-idxx = torch.zeros((1,1), dtype = torch.long).to(device=device)
-
-# print(decoder(m.generate(idxx, 1000)[0].tolist()))
-print(mkdata.decode(m.generate(idxx, 1000)[0].tolist()))
+tokenizer = Tokenizer(text)
+out = tokenizer("""r n b q k b n r
+p p p p p p p p
+. . . . . . . .
+. . . . . . . .
+. . . . . . . .
+. . . . . N . .
+P P P P P P P P
+R N B Q K B . R""")
+input_data = torch.tensor(out,dtype=torch.long).to(device=device)
+input_data = input_data.reshape(1,-1)
+print(mkdata.decode(m.generate(input_data, 1000)[0].tolist()))
